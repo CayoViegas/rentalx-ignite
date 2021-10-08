c243e849eff25a866051f9cf0df7622cba6281f7
CayoViegas <cayo.viegas@ccc.ufcg.edu.br>
2021-10-08 16:29:35 -0300

    metodo de cadastro de categoria implementado

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,11 @@
 import express from "express";
 
+import { categoriesRoutes } from "./routes/categories.routes";
+
 const app = express();
 
-app.get("/", (request, response) => {
-    return response.json({ message: "Hello world!" });
-});
+app.use(express.json());
+
+app.use(categoriesRoutes);
 
 app.listen(8001, () => console.log("Server is running!"));
