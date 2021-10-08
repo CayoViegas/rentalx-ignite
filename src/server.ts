3e042230ec7de541d5316608d49243ec4e53c1b6
CayoViegas <cayo.viegas@ccc.ufcg.edu.br>
2021-10-08 12:40:21 -0300

    novo projeto iniciado, padronizadores de codigo configurados

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,9 @@
-import express from 'express';
-import { createCourse } from './routes';
+import express from "express";
 
 const app = express();
 
-app.get("/", createCourse);
+app.get("/", (request, response) => {
+    return response.json({ message: "Hello world!" });
+});
 
-app.listen(8001);
+app.listen(8001, () => console.log("Server is running!"));
