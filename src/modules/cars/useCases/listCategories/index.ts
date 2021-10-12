ebdc5048cbd94ad9a1b972c4b5d0be538e6d3aef
CayoViegas <cayo.viegas@ccc.ufcg.edu.br>
2021-10-12 15:12:17 -0300

    refatoraçao da listagem de categorias e implementaçao do padrao singleton no repositorio

---
+++ src/modules/cars/useCases/listCategories/index.ts
@@ -0,0 +1,11 @@
+import { CategoriesRepository } from "../../repositories/CategoriesRepository";
+import { ListCategoriesController } from "./ListCategoriesController";
+import { ListCategoriesUserCase } from "./ListCategoriesUseCase";
+
+const categoriesRepository = CategoriesRepository.getInstance();
+const listCategoriesUseCase = new ListCategoriesUserCase(categoriesRepository);
+const listCategoriesController = new ListCategoriesController(
+    listCategoriesUseCase
+);
+
+export { listCategoriesController };
